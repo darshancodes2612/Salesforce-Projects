506e217fd3eb16dc0c29ee8c64a0ede4b2bd5268
darshancodes2612 <joshi.darshan1826@gmail.com>
2025-07-22 13:44:29 +0530

    adding accountSelector and customerDashboard

--- force-app/main/default/lwc/accountSelector/accountSelector.js
+++ force-app/main/default/lwc/accountSelector/accountSelector.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import showAccountList from '@salesforce/apex/SearchAccountList.showAccountList';
 
-export default class AccountSelector extends LightningElement {}
+export default class AccountSelector extends LightningElement {
+    @track queryTerm = '';
+    @track accounts = [];
+
+    generateAccountData(event) {
+        this.queryTerm = event.target.value;
+    }
+
+    @wire(showAccountList, { inputNameProvided: '$queryTerm' })
+    wiredAccount({ error, data }) {
+        if (data) {
+            this.accounts = data;
+        } else if (error) {
+            console.error("Error occurred:", error);
+            this.accounts = [];
+        }
+    }
+}
